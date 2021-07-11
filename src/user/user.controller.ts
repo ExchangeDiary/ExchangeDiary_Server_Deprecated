3cdbd96f9a32e2dbcf59d25ac0d959459a63910a
Heesu Suh <hssuh96@gmail.com>
2021-07-11 14:25:08 +0900

    dev 배포 세팅

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -7,11 +7,13 @@ import { UserService } from './user.service'
 export class UserController {
   constructor(private userService: UserService) {}
 
+  // TODO : 그냥 임시로 추가해둔 API. 나중에 삭제할 것
   @Post()
   async createUser(@Body() createUserDto: CreateUserDto): Promise<UserDto> {
     return this.userService.createUser(createUserDto)
   }
 
+  // TODO : 그냥 임시로 추가해둔 API. 나중에 삭제할 것
   @Get()
   async getUsers(): Promise<{ result: UserDto[] }> {
     const result = await this.userService.getUsers()
